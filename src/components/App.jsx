2de5516a4d66842acca475889befc09f57323153
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-22 15:58:21 +0600

    added profile component

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Profile from './profile';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Profile />
+      </div>
+    );
+  }
+}
+
+export default App;
