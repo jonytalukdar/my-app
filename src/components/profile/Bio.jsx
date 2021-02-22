4c8107c116dd2c134cae5119ae7ce4c3ff33529a
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-22 19:00:25 +0600

    make divide components

---
+++ src/components/profile/Bio.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Bio = () => (
+  <div className="Bio">
+    <h3>Joney Talukdar</h3>
+    <p>React developer and Designer</p>
+  </div>
+);
+
+export default Bio;
--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -1,39 +1,16 @@
 import React from 'react';
 import './profile.style.css';
+import Bio from './Bio';
+import Skills from './Skills';
+import Links from './Links';
 
 class Profile extends React.Component {
   render() {
     return (
       <div className="Main">
-        <div className="Bio">
-          <h3>Joney Talukdar</h3>
-          <p>React developer and Designer</p>
-        </div>
-        <div className="Skills">
-          <h3>Skills : </h3>
-          <ul>
-            <li>Java</li>
-            <li>Javascript</li>
-            <li>Python</li>
-          </ul>
-        </div>
-        <div className="Links">
-          <h3>Social Link</h3>
-          <ul>
-            <li>
-              <a href="#">Facebook</a>
-            </li>
-            <li>
-              <a href="#">Github</a>
-            </li>
-            <li>
-              <a href="#">Twitter</a>
-            </li>
-            <li>
-              <a href="#">LinkedIn</a>
-            </li>
-          </ul>
-        </div>
+        <Bio />
+        <Skills />
+        <Links />
       </div>
     );
   }
