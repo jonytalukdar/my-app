cf97232074f9d1ca377dccc4c64fe544776fcaec
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-23 11:13:43 +0600

    deleted all file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import Profile from './profile';
-import Skills from './profile/Skills';
-import Bio from './profile/Bio';
-// import MyProps from './props';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Profile />
-        <Bio name="Jerry Talukdar" title="Web Designer" />
-        <Skills skillA="PHP" skillB="MongoDB" skillC="Express" />
-        {/* <div> */}
-        {/* <MyProps name="Joney " />
-          <MyProps name="Jerry " />
-          <MyProps name="Jubayer " /> */}
-
-        {/* <p>Mr x</p>
-          <Skills />
-          <p>Mr y</p>
-          <Skills /> */}
-        {/* </div> */}
+        <h1>hello</h1>
       </div>
     );
   }
--- src/components/props/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const MyProps = (props) => {
-  return <h1>My name is {props.name} </h1>;
-};
-
-export default MyProps;
