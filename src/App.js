b67f65b1c21390c89b3b862642b56776fd268143
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-02 11:23:16 +0600

    added view update with state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.count = 2;
+    this.count = 5;
+    this.state = {
+      count: 0,
+    };
   }
 
+  // count = 10;
+
   render() {
-    console.log(this.count);
+    // console.log(this.count);
     return (
       <div>
         <h1>hello this is state</h1>
-        <h1>Count : {this.count}</h1>
+        <h1>Count : {this.state.count}</h1>
+        <button
+          onClick={() => {
+            // this.count++;
+            this.setState({ count: this.state.count + 1 });
+            console.log('clicked', this.state.count);
+          }}
+        >
+          Count Increase
+        </button>
       </div>
     );
   }
