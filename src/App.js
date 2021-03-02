da4792815cc1d471f4c4d505adeb9c2dd956eb4c
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-02 11:08:09 +0600

    added constructor properties

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.count = 2;
+  }
+
   render() {
+    console.log(this.count);
     return (
       <div>
         <h1>hello this is state</h1>
+        <h1>Count : {this.count}</h1>
       </div>
     );
   }
