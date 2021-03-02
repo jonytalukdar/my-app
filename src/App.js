e3daeef1e8c129cc0237b6580f0d3f3fb27ca66b
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-02 11:01:54 +0600

    added some code in App.js file

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import Cart from './components/cart/Cart';
-import Country from './components/country/Country';
+import React from 'react';
 
-const App = () => {
-  const [country, setCountry] = useState([]);
-  const [cart, setCart] = useState([]);
-
-  useEffect(() => {
-    fetch('https://restcountries.eu/rest/v2/all')
-      .then((response) => response.json())
-      .then((data) => setCountry(data));
-  }, []);
-
-  const handleClick = (country) => {
-    const newCart = [...cart, country];
-    setCart(newCart);
-  };
-
-  return (
-    <div>
-      <Cart cart={cart}></Cart>
-      <div className="App">
-        {country.map((countryDetails) => (
-          <Country
-            country={countryDetails}
-            handleClick={handleClick}
-            key={countryDetails.alpha3Code}
-          ></Country>
-        ))}
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>hello this is state</h1>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
