3825f352031733963418fa0a07398d592cd4282a
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-02 11:47:42 +0600

    added two function for increase and decrease

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  increaseCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decreaseCount = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
   render() {
     return (
       <div>
-        <h1>hello</h1>
+        <h1>Count : {this.state.count}</h1>
+        <button onClick={this.increaseCount}>increase</button>
+        <button onClick={this.decreaseCount}>decrease</button>
       </div>
     );
   }
