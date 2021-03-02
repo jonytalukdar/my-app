538585a5109ae91bbd113772fe64d06a9f23535b
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-02 15:26:39 +0600

    added react constructor for make clock dynamic

--- src/components/state/State.js
+++ src/components/state/State.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
 class State extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+    };
+    console.log(this.state.date);
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ date: new Date() });
+    }, 1000);
+  }
+
   render() {
     return (
       <div>
         <h1>
-          <span>{new Date().toLocaleString(this.props.locale)}</span>
+          <span>{this.state.date.toLocaleString(this.props.locale)}</span>
         </h1>
       </div>
     );
