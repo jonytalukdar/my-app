6eefe434e84ae16dd88e6a8f516d6eaf32b95aa4
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-02 12:18:43 +0600

    created a components for timer

--- src/App.js
+++ src/App.js
@@ -1,64 +1,9 @@
 import React from 'react';
+import Timer from './components/Timer/Timer';
 
 class App extends React.Component {
-  state = {
-    count: 0,
-  };
-
-  intervalId = null;
-
-  increaseCount = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  decreaseCount = () => {
-    if (this.state.count > 0) {
-      this.setState({ count: this.state.count - 1 });
-    }
-  };
-
-  startTimer = () => {
-    if (this.state.count > 0 && !this.intervalId) {
-      this.intervalId = setInterval(() => {
-        this.setState({ count: this.state.count - 1 }, () => {
-          if (this.state.count === 0) {
-            alert('Timer is finsihed');
-            clearInterval(this.intervalId);
-            this.intervalId = null;
-          }
-        });
-      }, 1000);
-    }
-  };
-
-  stopTimer = () => {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-      this.intervalId = null;
-    }
-  };
-
-  resetTimer = () => {
-    this.setState({ count: 0 });
-    clearInterval(this.intervalId);
-    this.intervalId = null;
-  };
-
   render() {
-    return (
-      <div style={{ textAlign: 'center' }}>
-        <h1>
-          {' '}
-          <span>{this.state.count}</span>
-        </h1>
-        <button onClick={this.decreaseCount}>-</button>
-        <button onClick={this.increaseCount}>+</button>
-        <br />
-        <button onClick={this.startTimer}>Start</button>
-        <button onClick={this.stopTimer}>Stop</button>
-        <button onClick={this.resetTimer}>Reset</button>
-      </div>
-    );
+    return <Timer></Timer>;
   }
 }
 
