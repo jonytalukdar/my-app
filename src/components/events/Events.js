be59365c7d3fb9a92d5b51a237811dc0c5ad5eb3
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-02 16:19:11 +0600

    added events basic

--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class Events extends Component {
+  handleButtonClick = (e) => {
+    console.log(e);
+    console.log(e.target);
+  };
   render() {
     return (
       <div>
         <h1>this is events</h1>
+        <button onClick={this.handleButtonClick}>Click me</button>
       </div>
     );
   }
