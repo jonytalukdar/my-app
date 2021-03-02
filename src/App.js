f409674f56f3c0ab2fd9b9876d7ae38d44626b1d
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-02 12:33:17 +0600

    added events components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import Timer from './components/Timer/Timer';
+import Events from './components/events/Events';
+// import Timer from './components/Timer/Timer';
 
 class App extends React.Component {
   render() {
-    return <Timer></Timer>;
+    return (
+      <div>
+        {' '}
+        <h1 style={{ textAlign: 'center' }}>Hello React</h1>
+        <Events></Events>
+      </div>
+    );
   }
 }
 
---
+++ src/components/events/Events.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Events extends Component {
+  render() {
+    return (
+      <div>
+        <h1>this is events</h1>
+      </div>
+    );
+  }
+}
+
+export default Events;
