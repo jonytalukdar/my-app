868dd598b346a0fab9e1fef006d55e3d475f5cc1
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-03 22:44:18 +0600

    added axios in useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Chart from './components/chart/Chart';
+// import Chart from './components/chart/Chart';
 // import Form from './components/formAndInput/Form';
 // import State from './components/state/State';
 // import Events from './components/events/Events';
 // import Timer from './components/Timer/Timer';
-// import Bootstrap from './components/bootstrap/Bootstrap';
+import Bootstrap from './components/bootstrap/Bootstrap';
 
 class App extends React.Component {
   render() {
@@ -12,7 +12,7 @@ class App extends React.Component {
       <div>
         {' '}
         <h1 style={{ textAlign: 'center' }}>Hello React</h1>
-        <Chart></Chart>
+        <Bootstrap></Bootstrap>
       </div>
     );
   }
--- src/components/bootstrap/components/Topheadline.js
+++ src/components/bootstrap/components/Topheadline.js
@@ -1,15 +1,22 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import News from './Card';
 
 const Topheadline = () => {
   const [articles, setArticles] = useState([]);
+  // useEffect(() => {
+  //   const url =
+  //     'https://newsapi.org/v2/top-headlines?country=us&apiKey=270e491750014e89a47d15979365477a';
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data));
+  // }, []);
+
   useEffect(() => {
-    fetch(
-      'https://newsapi.org/v2/top-headlines?country=us&apiKey=270e491750014e89a47d15979365477a'
-    )
-      .then((response) => response.json())
-      .then((data) => setArticles(data.articles));
-  }, []);
+    const url =
+      'https://newsapi.org/v2/top-headlines?country=us&apiKey=270e491750014e89a47d15979365477a';
+    axios(url).then((data) => setArticles(data.data.articles));
+  });
 
   const styleGrid = {
     display: 'grid',
