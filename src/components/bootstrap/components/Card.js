c791b3b4a2ddfaa825dc522acca308c454fc3ea5
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-03 11:53:00 +0600

    added react fragment

--- src/components/bootstrap/components/Card.js
+++ src/components/bootstrap/components/Card.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 const News = (props) => {
-  const { title, urlToImage, description } = props.article;
+  const { title, urlToImage, description, url } = props.article;
   return (
-    <div>
+    <>
       <Card>
         <Card.Img variant="top" src={urlToImage} />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{description}</Card.Text>
-          <Button variant="primary">Read More</Button>
+          <Button>
+            <a className="text-white" href={url}>
+              Read More
+            </a>
+          </Button>
         </Card.Body>
       </Card>
-    </div>
+    </>
   );
 };
 
--- src/components/bootstrap/components/Header.js
+++ src/components/bootstrap/components/Header.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import { Jumbotron, Button, Alert } from 'react-bootstrap';
+import { Jumbotron } from 'react-bootstrap';
 const Header = () => {
   return (
-    <Jumbotron>
-      <h1>Hello, world!</h1>
-      <p>
-        This is a simple hero unit, a simple jumbotron-style component for
-        calling extra attention to featured content or information.
-      </p>
-      <p>
-        <Button variant="primary">Learn more</Button>
-        <Alert variant="primary">hello</Alert>
-      </p>
+    <Jumbotron className="text-center">
+      <h1>Top News</h1>
     </Jumbotron>
   );
 };
