11f0b3b399f50edc386641f9d69560b0db98985e
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-08 16:40:01 +0600

    added some line in uncontrol form

--- src/components/formAndInput/UncontrolForm.js
+++ src/components/formAndInput/UncontrolForm.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import './uncontrol.css';
 
 class UncontrolForm extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {};
+    data.name = e.target.name.value;
+    data.email = e.target.email.value;
+    data.password = e.target.password.value;
+    console.log(data);
+    e.target.reset();
+  };
   render() {
     return (
-      <div>
-        <h1>hello this is uncontrol form</h1>
+      <div className="uncontrol" onSubmit={this.handleSubmit}>
+        <form>
+          <input
+            className="form-control"
+            type="text"
+            name="name"
+            placeholder="Joney Talukdar"
+          />
+          <input
+            className="form-control"
+            type="email"
+            name="email"
+            placeholder="Joneytalukdar30@gamil"
+          />
+          <input
+            className="form-control"
+            type="password"
+            name="password"
+            placeholder="********"
+          />{' '}
+          <button className="btn btn-primary" type="submit">
+            submit
+          </button>
+        </form>
       </div>
     );
   }
