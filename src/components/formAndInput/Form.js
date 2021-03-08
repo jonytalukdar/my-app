19d45f69c4d9d5a05239a0592012db5f8d32a503
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-08 12:51:12 +0600

    completed input form

--- src/components/formAndInput/Form.js
+++ src/components/formAndInput/Form.js
@@ -8,27 +8,53 @@ class Form extends Component {
     bio: '',
     birthday: '',
   };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
   render() {
+    const { name, country, bio, birthday } = this.state;
     return (
       <div className="input">
         <input
+          onChange={this.handleChange}
           className="form-control my-2"
           type="text"
           name="name"
+          value={name}
           placeholder="Enter Your Name"
         />
-        <select className="form-control my-2" name="country" id="">
+        <select
+          onChange={this.handleChange}
+          className="form-control my-2"
+          name="country"
+          value={country}
+          id=""
+        >
+          <option>Select Your Country</option>
           <option value="Bangladesh">Bangladesh</option>
           <option value="India">India</option>
           <option value="Pakistan">Pakistan</option>
           <option value="Sri-Lanka">Sri-Lanka</option>
         </select>
         <textarea
+          onChange={this.handleChange}
           className="form-control my-2"
           name="bio"
+          value={bio}
           placeholder="Tell About YourSelf"
         ></textarea>
-        <input className="form-control my-2" type="date" name="birthday" />
+        <input
+          onChange={this.handleChange}
+          className="form-control my-2"
+          type="date"
+          name="birthday"
+          value={birthday}
+        />
+        <button onClick={() => console.log(this.state)}> Show Data</button>
       </div>
     );
   }
