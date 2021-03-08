d18ff2885ff5d4cac90bdaa11fad654e12cd8ca4
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-08 12:27:59 +0600

    added form style

--- src/components/formAndInput/Form.js
+++ src/components/formAndInput/Form.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import './form.css';
 
 class Form extends Component {
   state = {
     name: '',
     country: '',
     bio: '',
+    birthday: '',
   };
   render() {
     return (
-      <div>
-        <h1>this is form input</h1>
+      <div className="input">
+        <input
+          className="form-control my-2"
+          type="text"
+          name="name"
+          placeholder="Enter Your Name"
+        />
+        <select className="form-control my-2" name="country" id="">
+          <option value="Bangladesh">Bangladesh</option>
+          <option value="India">India</option>
+          <option value="Pakistan">Pakistan</option>
+          <option value="Sri-Lanka">Sri-Lanka</option>
+        </select>
+        <textarea
+          className="form-control my-2"
+          name="bio"
+          placeholder="Tell About YourSelf"
+        ></textarea>
+        <input className="form-control my-2" type="date" name="birthday" />
       </div>
     );
   }
